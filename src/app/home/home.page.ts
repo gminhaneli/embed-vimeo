a642dfa94410cacff96631b4cf8ada8d141c5269
Gustavo Minhaneli <minhaneli2015@gmail.com>
2020-05-30 10:57:02 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {}
 
 }
